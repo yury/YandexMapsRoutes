1a3188bc2d06a9ceb333d5e0a6407e3d2e64ec0c
fr1que <a.suslin92@gmail.com>
2018-07-25 00:36:26 +0300

    added function for generating the coordinates of the airplane route

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,21 @@
 ymaps.ready(init);
 
-function init() {
-    var yMap = new ymaps.Map("map", {
-            center: [56.13, 40.40],
-            zoom: 10
+function init() 
+{
+    var maxPoints = 30,
+        yMap = new ymaps.Map("map", {
+		    center  : [56.13, 40.4],
+		    zoom    : 10
         });
+        
+    function getNewCoordinatesPlane(myPolyline)
+    {
+        let lengthGeometry = myPolyline.geometry.getLength(),
+            lastCoordinates = myPolyline.geometry.get(lengthGeometry - 1);
+
+        return lengthGeometry == 0
+        ? [56.13, 40.4]
+        : lastCoordinates.map(coord => coord - Math.random() / 100 );
+    }
 
 }
