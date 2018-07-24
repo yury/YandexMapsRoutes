5b980ea53797b8761ee5e551378e0f59851d349f
fr1que <a.suslin92@gmail.com>
2018-07-25 00:38:59 +0300

    added the route of the aircraft using setInterval

--- js/routes.js
+++ js/routes.js
@@ -18,4 +18,36 @@ function init()
         : lastCoordinates.map(coord => coord - Math.random() / 100 );
     }
 
+    function setNewGeometry(myPolyline, maxPoints)
+	{
+		let lengthGeometry = myPolyline.geometry.getLength(),
+			NewCoordinatesPlane = getNewCoordinatesPlane(myPolyline),
+			boundsPolyline;
+
+	    myPolyline.geometry.set(lengthGeometry, NewCoordinatesPlane);
+	    lengthGeometry = myPolyline.geometry.getLength();
+
+	    if (lengthGeometry - maxPoints > 0)
+	    	myPolyline.geometry.splice(0, lengthGeometry - maxPoints - 1);
+
+	    boundsPolyline = myPolyline.geometry.getBounds();
+
+	    yMap.setBounds(boundsPolyline, {
+	    	checkZoomRange : true,
+	    	duration       : 100,
+	    	zoomMargin     : 100
+	    });
+	}
+
+	let myPolyline = new ymaps.Polyline([], null, {
+		balloonCloseButton : false,
+		strokeColor        : "#0687b6",
+		strokeWidth        : 5,
+		strokeOpacity      : 0.7
+  	});
+
+	yMap.geoObjects.add(myPolyline);
+
+	setInterval( () => setNewGeometry(myPolyline, maxPoints), 1000);
+        
 }
